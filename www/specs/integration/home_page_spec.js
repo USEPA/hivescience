71eabe8a1c0d5e858ba5d8f8ae327017409dab6d
Feroz Daraseng daraseng.feroz@epa.gov and Joey Butler jbutler@pivotal.io <pivotal@Pivotals-iMac-5.local>
2016-12-14 15:48:58 -0500

    initial commit

---
+++ www/specs/integration/home_page_spec.js
@@ -0,0 +1,14 @@
+import ghost from 'ghostjs'
+import expect from 'expect.js'
+
+describe('Home page', () => {
+  it('loads when the device is ready', async () => {
+    expect(1).to.be.ok();
+    await ghost.open('http://localhost:3333');
+    let pageTitle = await ghost.pageTitle();
+    expect(pageTitle).to.equal('Hello World');
+
+    let body = await ghost.findElement('body');
+    expect(await body.isVisible()).to.be.ok();
+  })
+});
