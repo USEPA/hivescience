6386307685dbf8b83fc1e28f83926b6a81571659
Daniel Joyce djoyce@pivotal.io <pivotal@Pivotals-iMac-5.local>
2016-12-20 15:46:58 -0500

    Remove platforms and plugins dirs.

--- platforms/browser/www/js/helpers.js
+++
@@ -1,8 +0,0 @@
-import _ from "underscore"
-
-export const formatAttributes = (attributes) => {
-  return _.reduce(attributes, (acc, elem) => {
-    acc[elem.name] = elem.value;
-    return acc;
-  }, {});
-};
