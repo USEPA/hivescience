ba66b6f5c2006870e23696ffc259c5088d368c87
Daniel Joyce djoyce@pivotal.io and Feroz Daraseng daraseng.feroz@epa.gov <pivotal@monroe.local>
2016-12-21 13:47:45 -0500

    Belinda can enter how many times she's has monitored for varroa mites in the past year

--- www/js/db.js
+++ www/js/db.js
@@ -14,7 +14,8 @@ export default class DB {
       "CREATE TABLE IF NOT EXISTS profiles" +
       "(id INTEGER PRIMARY KEY AUTOINCREMENT, email VARCHAR(100), " +
       "full_name VARCHAR(100), zip_code VARCHAR(20), " +
-      "number_of_colonies INTEGER, monitor_varroa_mites VARCHAR(1));",
+      "number_of_colonies INTEGER, monitor_varroa_mites VARCHAR(1), " +
+      "monitor_varroa_mites_count INTEGER);",
       [],
       () => {
         console.log('create profiles table successful')
@@ -27,12 +28,12 @@ export default class DB {
 
   createProfile(attributes) {
     const values = [attributes.email, attributes.fullName, attributes.zipCode,
-      attributes.numberOfColonies, attributes.monitorVarroaMites];
+      attributes.numberOfColonies, attributes.monitorVarroaMites, attributes.monitorVarroaMitesCount];
 
     this.connection.executeSql(
       "INSERT INTO profiles " +
-      "(email, full_name, zip_code, number_of_colonies, monitor_varroa_mites) " +
-      "VALUES (?, ?, ?, ?, ?);",
+      "(email, full_name, zip_code, number_of_colonies, monitor_varroa_mites, monitor_varroa_mites_count) " +
+      "VALUES (?, ?, ?, ?, ?, ?);",
       values,
       () => {
         console.log('profile insert successful')
--- www/specs/unit/db_spec.js
+++ www/specs/unit/db_spec.js
@@ -5,7 +5,8 @@ import DB from "../../js/db";
 
 describe("DB", () => {
   const connection = {
-    executeSql: () => {}
+    executeSql: () => {
+    }
   };
   const sqlitePlugin = {
     openDatabase: () => connection
@@ -40,7 +41,8 @@ describe("DB", () => {
       let sqlStatement = "CREATE TABLE IF NOT EXISTS profiles" +
         "(id INTEGER PRIMARY KEY AUTOINCREMENT, email VARCHAR(100), " +
         "full_name VARCHAR(100), zip_code VARCHAR(20), " +
-        "number_of_colonies INTEGER, monitor_varroa_mites VARCHAR(1));";
+        "number_of_colonies INTEGER, monitor_varroa_mites VARCHAR(1), " +
+        "monitor_varroa_mites_count INTEGER);";
       sinon.assert.calledWithMatch(executeSqlSpy, sqlStatement);
     });
   });
@@ -52,15 +54,17 @@ describe("DB", () => {
         fullName: "Belinda Bees",
         zipCode: "12345-5555",
         numberOfColonies: 111,
-        monitorVarroaMites: "N"
+        monitorVarroaMites: "N",
+        monitorVarroaMitesCount: 345
       };
       db.createProfile(attributes);
       const sqlStatement = "INSERT INTO profiles " +
-        "(email, full_name, zip_code, number_of_colonies, monitor_varroa_mites) " +
-        "VALUES (?, ?, ?, ?, ?);";
+        "(email, full_name, zip_code, number_of_colonies, monitor_varroa_mites, monitor_varroa_mites_count) " +
+        "VALUES (?, ?, ?, ?, ?, ?);";
       sinon.assert.calledWithMatch(executeSqlSpy, sqlStatement,
         [attributes.email, attributes.fullName, attributes.zipCode,
-         attributes.numberOfColonies, attributes.monitorVarroaMites]);
+          attributes.numberOfColonies, attributes.monitorVarroaMites,
+          attributes.monitorVarroaMitesCount]);
     });
   });
 });
