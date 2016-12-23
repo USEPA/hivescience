f58c594e5572cbd4523caed9d12d817fdf4462dc
Daniel Joyce djoyce@pivotal.io and Feroz Daraseng daraseng.feroz@epa.gov <pivotal@monroe.local>
2016-12-23 16:39:20 -0500

    Clean up sql statements for readability

--- www/js/db.js
+++ www/js/db.js
@@ -4,9 +4,10 @@ export default class DB {
   }
 
   initialize() {
-    this.connection = this.sqlitePlugin.openDatabase(
-      {name: "buzz_buzz", location: "default"}
-    );
+    this.connection = this.sqlitePlugin.openDatabase({
+      name: "buzz_buzz",
+      location: "default"
+    });
   }
 
   createTables() {
@@ -15,13 +16,21 @@ export default class DB {
   }
 
   createProfilesTable() {
+    const sqlStatement = `
+      CREATE TABLE IF NOT EXISTS profiles (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        email VARCHAR(100),
+        full_name VARCHAR(100),
+        zip_code VARCHAR(20),
+        number_of_colonies INTEGER,
+        monitor_varroa_mites VARCHAR(1),
+        monitor_varroa_mites_count INTEGER,
+        monitor_methods VARCHAR(255),
+        treatment_methods VARCHAR(255),
+        why_beekeep TEXT
+      );`;
     this.connection.executeSql(
-      "CREATE TABLE IF NOT EXISTS profiles" +
-      "(id INTEGER PRIMARY KEY AUTOINCREMENT, email VARCHAR(100), " +
-      "full_name VARCHAR(100), zip_code VARCHAR(20), " +
-      "number_of_colonies INTEGER, monitor_varroa_mites VARCHAR(1), " +
-      "monitor_varroa_mites_count INTEGER, monitor_methods VARCHAR(255), " +
-      "treatment_methods VARCHAR(255), why_beekeep TEXT);",
+      sqlStatement,
       [],
       () => console.log('create profiles table successful'),
       () => console.log('create profiles table failed')
@@ -29,14 +38,23 @@ export default class DB {
   }
 
   createSurveysTable() {
+    const sqlStatement = `
+      CREATE TABLE IF NOT EXISTS surveys (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        queen_right VARCHAR(1),
+        queen_drone_laying VARCHAR(1),
+        queen_age INTEGER,
+        diseases TEXT,
+        bee_kill VARCHAR(1),
+        bee_kill_description TEXT,
+        honey_supers_on VARCHAR(1),
+        honey_from_sealed_cells VARCHAR(1),
+        honey_from_brood VARCHAR(1),
+        split_or_combine VARCHAR(1),
+        sample_tube_code INTEGER
+      );`;
     this.connection.executeSql(
-      "CREATE TABLE IF NOT EXISTS surveys" +
-      "(id INTEGER PRIMARY KEY AUTOINCREMENT, queen_right VARCHAR(1), " +
-      "queen_drone_laying VARCHAR(1), queen_age INTEGER, " +
-      "diseases TEXT, bee_kill VARCHAR(1), bee_kill_description TEXT, " +
-      "honey_supers_on VARCHAR(1), honey_from_sealed_cells VARCHAR(1), " +
-      "honey_from_brood VARCHAR(1), split_or_combine VARCHAR(1), " +
-      "sample_tube_code INTEGER);",
+      sqlStatement,
       [],
       () => console.log('create surveys table successful'),
       () => console.log('create surveys table failed')
@@ -44,17 +62,32 @@ export default class DB {
   }
 
   createProfile(attributes) {
-    const values = [attributes.email, attributes.fullName, attributes.zipCode,
-      attributes.numberOfColonies, attributes.monitorVarroaMites,
-      attributes.monitorVarroaMitesCount, attributes.monitorMethods,
-      attributes.treatmentMethods, attributes.whyBeekeep];
-
+    const sqlStatement = `
+        INSERT INTO profiles (
+          email,
+          full_name,
+          zip_code,
+          number_of_colonies,
+          monitor_varroa_mites,
+          monitor_varroa_mites_count,
+          monitor_methods,
+          treatment_methods,
+          why_beekeep
+        )
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);`;
+    const values = [
+      attributes.email,
+      attributes.fullName,
+      attributes.zipCode,
+      attributes.numberOfColonies,
+      attributes.monitorVarroaMites,
+      attributes.monitorVarroaMitesCount,
+      attributes.monitorMethods,
+      attributes.treatmentMethods,
+      attributes.whyBeekeep
+    ];
     this.connection.executeSql(
-      "INSERT INTO profiles " +
-      "(email, full_name, zip_code, number_of_colonies, monitor_varroa_mites, " +
-      "monitor_varroa_mites_count, monitor_methods, treatment_methods, " +
-      "why_beekeep) " +
-      "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);",
+      sqlStatement,
       values,
       () => console.log('profile insert successful'),
       (error) => console.log('profile insert failed', error)
@@ -62,17 +95,36 @@ export default class DB {
   }
 
   createSurvey(attributes) {
-    const values = [attributes.queenRight, attributes.queenDroneLaying,
-      attributes.queenAge, attributes.diseases, attributes.beeKill,
-      attributes.beeKillDescription, attributes.honeySupersOn,
-      attributes.honeyFromSealedCells, attributes.honeyFromBrood,
-      attributes.splitOrCombine, attributes.sampleTubeCode];
+    const sqlStatement = `
+        INSERT INTO surveys (
+          queen_right,
+          queen_drone_laying,
+          queen_age,
+          diseases,
+          bee_kill,
+          bee_kill_description,
+          honey_supers_on,
+          honey_from_sealed_cells,
+          honey_from_brood,
+          split_or_combine,
+          sample_tube_code
+        )
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`;
+    const values = [
+      attributes.queenRight,
+      attributes.queenDroneLaying,
+      attributes.queenAge,
+      attributes.diseases,
+      attributes.beeKill,
+      attributes.beeKillDescription,
+      attributes.honeySupersOn,
+      attributes.honeyFromSealedCells,
+      attributes.honeyFromBrood,
+      attributes.splitOrCombine,
+      attributes.sampleTubeCode
+    ];
     this.connection.executeSql(
-      "INSERT INTO surveys " +
-      "(queen_right, queen_drone_laying, queen_age, diseases, bee_kill, bee_kill_description," +
-      "honey_supers_on, honey_from_sealed_cells, honey_from_brood, split_or_combine, " +
-      "sample_tube_code) " +
-      "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);",
+      sqlStatement,
       values,
       () => console.log('survey insert successful'),
       (error) => console.log('survey insert failed', error)
--- www/specs/unit/db_spec.js
+++ www/specs/unit/db_spec.js
@@ -38,24 +38,39 @@ describe("DB", () => {
 
     it("creates the profile table for this app", () => {
       db.createTables();
-      const sqlStatement = "CREATE TABLE IF NOT EXISTS profiles" +
-        "(id INTEGER PRIMARY KEY AUTOINCREMENT, email VARCHAR(100), " +
-        "full_name VARCHAR(100), zip_code VARCHAR(20), " +
-        "number_of_colonies INTEGER, monitor_varroa_mites VARCHAR(1), " +
-        "monitor_varroa_mites_count INTEGER, monitor_methods VARCHAR(255), " +
-        "treatment_methods VARCHAR(255), why_beekeep TEXT);";
+      const sqlStatement = `
+      CREATE TABLE IF NOT EXISTS profiles (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        email VARCHAR(100),
+        full_name VARCHAR(100),
+        zip_code VARCHAR(20),
+        number_of_colonies INTEGER,
+        monitor_varroa_mites VARCHAR(1),
+        monitor_varroa_mites_count INTEGER,
+        monitor_methods VARCHAR(255),
+        treatment_methods VARCHAR(255),
+        why_beekeep TEXT
+      );`;
       sinon.assert.calledWithMatch(executeSqlSpy, sqlStatement);
     });
 
     it("creates the survey table for this app", () => {
       db.createTables();
-      const sqlStatement = "CREATE TABLE IF NOT EXISTS surveys" +
-        "(id INTEGER PRIMARY KEY AUTOINCREMENT, queen_right VARCHAR(1), " +
-        "queen_drone_laying VARCHAR(1), queen_age INTEGER, " +
-        "diseases TEXT, bee_kill VARCHAR(1), bee_kill_description TEXT, " +
-        "honey_supers_on VARCHAR(1), honey_from_sealed_cells VARCHAR(1), " +
-        "honey_from_brood VARCHAR(1), split_or_combine VARCHAR(1), " +
-        "sample_tube_code INTEGER);";
+      const sqlStatement = `
+      CREATE TABLE IF NOT EXISTS surveys (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        queen_right VARCHAR(1),
+        queen_drone_laying VARCHAR(1),
+        queen_age INTEGER,
+        diseases TEXT,
+        bee_kill VARCHAR(1),
+        bee_kill_description TEXT,
+        honey_supers_on VARCHAR(1),
+        honey_from_sealed_cells VARCHAR(1),
+        honey_from_brood VARCHAR(1),
+        split_or_combine VARCHAR(1),
+        sample_tube_code INTEGER
+      );`;
       sinon.assert.calledWithMatch(executeSqlSpy, sqlStatement);
     });
   });
@@ -63,6 +78,20 @@ describe("DB", () => {
   describe("create", () => {
     describe("createProfile", () => {
       it("persists a profile record with the provided attributes", () => {
+        const sqlStatement = `
+        INSERT INTO profiles (
+          email,
+          full_name,
+          zip_code,
+          number_of_colonies,
+          monitor_varroa_mites,
+          monitor_varroa_mites_count,
+          monitor_methods,
+          treatment_methods,
+          why_beekeep
+        )
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);`;
+
         const attributes = {
           email: "belinda@beekeepers.us",
           fullName: "Belinda Bees",
@@ -77,21 +106,40 @@ describe("DB", () => {
 
         db.createProfile(attributes);
 
-        const sqlStatement = "INSERT INTO profiles " +
-          "(email, full_name, zip_code, number_of_colonies, monitor_varroa_mites, " +
-          "monitor_varroa_mites_count, monitor_methods, treatment_methods, " +
-          "why_beekeep) " +
-          "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);";
-        sinon.assert.calledWithMatch(executeSqlSpy, sqlStatement,
-          [attributes.email, attributes.fullName, attributes.zipCode,
-            attributes.numberOfColonies, attributes.monitorVarroaMites,
-            attributes.monitorVarroaMitesCount, attributes.monitorMethods,
-            attributes.treatmentMethods, attributes.whyBeekeep]);
+        const expectedAttributes = [
+          attributes.email,
+          attributes.fullName,
+          attributes.zipCode,
+          attributes.numberOfColonies,
+          attributes.monitorVarroaMites,
+          attributes.monitorVarroaMitesCount,
+          attributes.monitorMethods,
+          attributes.treatmentMethods,
+          attributes.whyBeekeep
+        ];
+
+        sinon.assert.calledWithMatch(executeSqlSpy, sqlStatement, expectedAttributes);
       });
     });
 
     describe("createSurvey", () => {
       it("persists a survey record with the provided attributes", () => {
+        const sqlStatement = `
+        INSERT INTO surveys (
+          queen_right,
+          queen_drone_laying,
+          queen_age,
+          diseases,
+          bee_kill,
+          bee_kill_description,
+          honey_supers_on,
+          honey_from_sealed_cells,
+          honey_from_brood,
+          split_or_combine,
+          sample_tube_code
+        )
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`;
+
         const attributes = {
           queenRight: "Y",
           queenDroneLaying: "N",
@@ -108,17 +156,21 @@ describe("DB", () => {
 
         db.createSurvey(attributes);
 
-        const sqlStatement = "INSERT INTO surveys " +
-          "(queen_right, queen_drone_laying, queen_age, diseases, bee_kill, bee_kill_description," +
-          "honey_supers_on, honey_from_sealed_cells, honey_from_brood, split_or_combine, " +
-          "sample_tube_code) " +
-          "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
-        sinon.assert.calledWithMatch(executeSqlSpy, sqlStatement,
-          [attributes.queenRight, attributes.queenDroneLaying,
-            attributes.queenAge, attributes.diseases, attributes.beeKill,
-            attributes.beeKillDescription, attributes.honeySupersOn,
-            attributes.honeyFromSealedCells, attributes.honeyFromBrood,
-            attributes.splitOrCombine, attributes.sampleTubeCode]);
+        const expectedAttributes = [
+          attributes.queenRight,
+          attributes.queenDroneLaying,
+          attributes.queenAge,
+          attributes.diseases,
+          attributes.beeKill,
+          attributes.beeKillDescription,
+          attributes.honeySupersOn,
+          attributes.honeyFromSealedCells,
+          attributes.honeyFromBrood,
+          attributes.splitOrCombine,
+          attributes.sampleTubeCode
+        ];
+
+        sinon.assert.calledWithMatch(executeSqlSpy, sqlStatement, expectedAttributes);
       });
     });
   });
